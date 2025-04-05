8a8f9de81dd841fef33409288a1a8d876d4ab61b
Greyson Tran <apple.greyson.tran@gmail.com>
2025-04-05 16:47:48 -0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,199 +1,66 @@
-use std::f64::consts::*;
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write, stdin, stdout};
-use std::mem::transmute;
+use std::result;
 
-fn main() {
-    let mut serial_interface = USB::new("/dev/tty.usbmodem.XPS1");
+mod usb;
 
-    loop {
-        stdout().flush().unwrap();
+const MAXIMUM_AS_BYTES: [u8; 4] = f32::MAX.to_le_bytes();
+const MINIMUM_AS_BYTES: [u8; 4] = f32::MIN.to_le_bytes();
 
-        let mut instruction = String::new();
-        stdin()
-            .read_line(&mut instruction)
-            .expect("macOS Spesific Error");
+fn main() -> () {}
 
-        let command: Vec<&str> = instruction.trim().split_whitespace().collect();
-
-        if command.len() != 3 {
-            continue;
-        }; // Ensure Propper Command Formatting
-
-        match command[0] {
-            "cntr" => {
-                serial_interface.send_instruction((f32::MAX as f64, f32::MAX as f64)); // Enter Configuration Mode
-                serial_interface.block_for_next(); // Wait for Synchronize
-
-                let arg_one = match command[1].parse() {
-                    Ok(f64) => f64,
-                    _ => continue,
-                };
-
-                let arg_two = match command[2].parse() {
-                    Ok(f64) => f64,
-                    _ => continue,
-                };
-
-                serial_interface.send_instruction((arg_one, arg_two)); // CONFIG: MoveServo
-                serial_interface.block_for_next(); // Wait for Synchronize
-            }
-
-            "home" => {
-                serial_interface.send_instruction((f32::MAX as f64, f32::MAX as f64)); // Enter Configuration Mode
-                serial_interface.block_for_next(); // Wait for Synchronize
-                serial_interface.send_instruction((f32::MAX as f64, f32::MAX as f64)); // CONFIG: Home
-                serial_interface.block_for_next(); // Wait for Synchronize
-            }
-
-            "gt" => {
-                let arg_one = match command[1].parse() {
-                    Ok(f64) => f64,
-                    _ => continue,
-                };
-
-                let arg_two = match command[2].parse() {
-                    Ok(f64) => f64,
-                    _ => continue,
-                };
-
-                serial_interface.send_instruction((arg_one, arg_two)); // Send Instruction / Go-To / G1
-                serial_interface.block_for_next(); // Wait for Synchronize
-            }
-
-            _ => continue,
-        };
+fn decode_command(command: [String; 3]) -> [[u8; 8]; 2] {
+    match command[0].as_str() {
+        "sthm" => set_home(),
+        _ => panic!(""),
     }
 }
 
-// Grouped Mathematical Operations. Relevant Feature Set only.
-struct Math;
-impl Math {
-    fn acos_rule(a: f64, b: f64, c: f64) -> f64 {
-        Self::to_degrees(((a.powf(2.0) + b.powf(2.0) - c.powf(2.0)) / (2.0 * a * b)).acos())
-    }
-
-    fn cos_rule(a: f64, b: f64, angle: f64) -> f64 {
-        (a.powf(2.0) + b.powf(2.0) - 2.0 * a * b * (Self::to_radians(angle)).cos()).powf(0.5)
-    }
-
-    fn to_radians(angle: f64) -> f64 {
-        angle / 180.0 * PI
-    }
-
-    fn to_degrees(angle: f64) -> f64 {
-        angle / PI * 180.0
-    }
-
-    fn distance(origin: (f64, f64), destination: (f64, f64)) -> f64 {
-        (origin.0 - destination.0) + (origin.1 - destination.1)
-    }
+fn set_home() -> [[u8; 8]; 2] {
+    [
+        protected_to_long(&MAXIMUM_AS_BYTES, &MAXIMUM_AS_BYTES), // Enter "Special Mode"
+        [0u8; 8],
+    ]
 }
 
-struct XPS {
-    motor_length: f64,
-    proximal_length: f64,
-    distal_length: f64,
-    pen_holder_length: f64,
+fn go_to(x: f32, y: f32) -> [[u8; 8]; 2] {
+    [
+        protected_to_long(&x.to_le_bytes(), &y.to_le_bytes()),
+        [0u8; 8],
+    ]
 }
 
-impl XPS {
-    fn new(
-        motor_length: f64,
-        proximal_length: f64,
-        distal_length: f64,
-        pen_holder_length: f64,
-    ) -> Self {
-        Self {
-            motor_length,
-            proximal_length,
-            distal_length,
-            pen_holder_length,
-        }
-    }
-
-    fn inverse_kinematics(&self, destination: (f64, f64)) -> (f64, f64) {
-        // Angle, Motor Zero / Angle, Motor One
-        // Angle Notation / Motor Enum
-        // Alpha: MotorLine / Shortest-Path-First
-        // Beta: Shortest-Path-First / Proximal
-        // Gamma: Proximal / Distal
-
-        let gamma_zero = {
-            let motor_zero_pen = Math::distance((0.0, 0.0), destination);
-            Math::acos_rule(
-                self.proximal_length,
-                self.distal_length + self.pen_holder_length,
-                motor_zero_pen,
-            )
-        };
-
-        let gamma_one = {
-            let motor_one_pen = Math::distance((self.motor_length, 0.0), destination);
-            Math::acos_rule(
-                self.proximal_length,
-                self.distal_length + self.pen_holder_length,
-                motor_one_pen,
-            )
-        };
-
-        let motor_zero_common =
-            Math::cos_rule(self.proximal_length, self.distal_length, gamma_zero);
-        let motor_one_common = Math::cos_rule(self.proximal_length, self.distal_length, gamma_one);
-
-        let beta_zero =
-            Math::acos_rule(self.proximal_length, motor_zero_common, self.distal_length);
-        let beta_one = Math::acos_rule(self.proximal_length, motor_one_common, self.distal_length);
-
-        let alpha_zero = Math::acos_rule(self.motor_length, motor_zero_common, motor_one_common);
-        let alpha_one = Math::acos_rule(self.motor_length, motor_one_common, motor_zero_common);
-
-        (alpha_zero + beta_zero, alpha_one + beta_one)
-    }
+fn toolhead_up() -> [[u8; 8]; 2] {
+    [
+        protected_to_long(&MAXIMUM_AS_BYTES, &MAXIMUM_AS_BYTES), // Enter "Special Mode"
+        protected_to_long(&MAXIMUM_AS_BYTES, &MAXIMUM_AS_BYTES), // Send "High Command"
+    ]
 }
 
-struct USB<'a> {
-    path: &'a str,
-    port: File,
+fn toolhead_down() -> [[u8; 8]; 2] {
+    [
+        protected_to_long(&MAXIMUM_AS_BYTES, &MAXIMUM_AS_BYTES), // Enter "Special Mode"
+        protected_to_long(&MINIMUM_AS_BYTES, &MINIMUM_AS_BYTES), // Send "Low Command"
+    ]
 }
 
-impl<'a> USB<'a> {
-    fn new(path: &'a str) -> Self {
-        Self {
-            path: path,
-            port: OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(path)
-                .expect("macOS Spesific Error / Device Not Detected"),
-        }
-    }
-
-    fn send_instruction(&mut self, double_precision_angles: (f64, f64)) {
-        let single_precision_angles = (
-            double_precision_angles.0 as f32,
-            double_precision_angles.1 as f32,
-        );
-
-        let command: [u8; 8] = unsafe {
-            transmute([
-                single_precision_angles.0.to_le_bytes(),
-                single_precision_angles.1.to_le_bytes(),
-            ])
-        };
-
-        self.port
-            .write_all(&command)
-            .expect("Interface Failure in Forward Direction");
-    }
-
-    fn block_for_next(&mut self) {
-        let mut responce = [0u8; 8];
-
-        match self.port.read_exact(&mut responce) {
-            Ok(()) if &responce == b"INSTRCTN" => return,
-            Ok(()) => panic!("Bad Traffic on Interface"),
-            Err(_) => panic!("Interface Failure in Backward Direction"),
-        }
-    }
+fn protected_to_long(a: &[u8; 4], b: &[u8; 4]) -> [u8; 8] {
+    let mut result = [0u8; 8];
+    result[..4].copy_from_slice(a);
+    result[4..].copy_from_slice(b);
+    result
 }
+
+fn long_to_protected(a: &[u8; 8]) -> [[u8; 4]; 2] {
+    let mut result_zero = [0u8; 4];
+    let mut result_one = [0u8; 4];
+
+    result_zero.copy_from_slice(&a[..4]);
+    result_one.copy_from_slice(&a[4..]);
+
+    [result_zero, result_one]
+}
+// Commands
+// ---
+// STHM - SeT HoMe
+// GT - Go To
+// THUP - ToolHead UP
+// THDN - ToolHead DowN
---
+++ src/usb.rs
@@ -0,0 +1,33 @@
+use serialport::SerialPort;
+use std::io::{stdin, stdout};
+
+pub struct USB {
+    port: Box<dyn SerialPort>,
+}
+
+impl USB {
+    pub fn new() -> Self {
+        Self {
+            port: serialport::new("/dev/tty.usbmodemXPSF1", 9600)
+                .timeout(std::time::Duration::from_secs(1))
+                .open()
+                .expect("A macOS Spesific Error Has Occured / Failed to Start Interface"),
+        }
+    }
+
+    pub fn send_instruction(&mut self, command: [u8; 8]) {
+        self.port
+            .write_all(&command)
+            .expect("A macOS Spesific Error Has Occured / Failed to Send Instruction");
+    }
+
+    pub fn block_for_next(&mut self) {
+        let mut responce = [0u8; 8];
+
+        match self.port.read_exact(&mut responce) {
+            Ok(()) if &responce == b"INSTRCTN" => return,
+            Ok(()) => panic!("Bad Traffic on Interface"),
+            Err(_) => panic!("Interface Failure in Backward Direction"),
+        }
+    }
+}
